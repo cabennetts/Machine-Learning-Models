b4d818e4338bfc5b9fa7c585fb7ea8cabbfc820e
Caleb <48822049+cabennetts@users.noreply.github.com>
2022-12-18 23:24:44 -0600

    added 4 new classifiers
    - kNeighbors
    -Linear Discriminant Analysis
    - Quadratic Discriminant Analysis
    - Naive Bayesian Classifier

---
+++ NaiveBayesianClassifier.py
@@ -0,0 +1,45 @@
+import sys
+import scipy
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import cross_val_score, train_test_split, RepeatedStratifiedKFold, cross_val_score
+from sklearn.naive_bayes import GaussianNB # Naive Bayesian
+
+# loads the iris data set
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv('./iris.csv', names=names)
+array = dataset.values # turn numpy dataset into array of values
+
+X = array[:,0:4] # contains flower features
+y = array[:,4] # contains flower names
+
+# convert class from strings to integers for training regression models
+array2 = preprocessing.LabelEncoder()
+array2.fit(y)
+array2.transform(y)
+
+# Split data into 2 folds for training and test
+X_train, X_test, y_train, y_test = train_test_split(X, array2.transform(y), test_size=0.50, random_state=1)
+
+# Naive Bayesian
+def NaiveBayes_Test(x1, x2, y1, y2):
+
+  model = GaussianNB()
+  model.fit(x1, y1) # first fold training
+  pred1 = model.predict(x2) # first fold testing
+  model.fit(x2, y2) # second fold training
+  pred2 = model.predict(x1) # second fold testing
+
+  # Evaluate Model
+  actual = np.concatenate([y2, y1]) # actual classes
+  predicted = np.concatenate([pred1,pred2]) # predicted classes
+  predicted = predicted.round()
+
+  print("Naive Bayesian:")
+  print("Accuracy Score: ",accuracy_score(actual, predicted)) # accuracy
+  print("Confusion Matrix:\n",confusion_matrix(actual, predicted)) # confusion matrix
+
+NaiveBayes_Test(X_train, X_test, y_train, y_test)
