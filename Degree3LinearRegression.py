2caa59e0d0cb81f33b2dcc5731073834b36afe1a
Caleb <48822049+cabennetts@users.noreply.github.com>
2022-12-18 23:17:27 -0600

    Fixed degree 3
    Linear Regression of degree 3 now works

--- Degree3LinearRegression.py
+++ Degree3LinearRegression.py
@@ -4,11 +4,22 @@ import numpy as np
 import pandas as pd
 import sklearn
 from sklearn import preprocessing
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import cross_val_score, train_test_split, RepeatedStratifiedKFold, cross_val_score
+
 from sklearn.linear_model import LinearRegression # linear regression
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler # polynomial regression
 from sklearn.pipeline import Pipeline # for polynomial
-from sklearn.model_selection import cross_val_score, train_test_split, RepeatedStratifiedKFold, cross_val_score
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.naive_bayes import GaussianNB # Naive Bayesian
+from sklearn.neighbors import KNeighborsClassifier #kNN
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+
+# added to v2
+from sklearn.svm import SVC # svm
+from sklearn import tree # decision tree 
+from sklearn.ensemble import RandomForestClassifier # random forest
+from sklearn.ensemble import ExtraTreesClassifier # extra trees
+from sklearn.neural_network import MLPClassifier # neural network 
 
 # loads the iris data set
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
@@ -16,8 +27,15 @@ dataset = pd.read_csv('./iris.csv', names=names)
 array = dataset.values # turn numpy dataset into array of values
 
 X = array[:,0:4] # contains flower features
-print(X)
 y = array[:,4] # contains flower names
+
+Parr3 = preprocessing.LabelEncoder()
+PR3 = PolynomialFeatures(degree=6)
+PR3.fit_transform(X)
+Parr3.fit(y)
+Parr3.transform(y)
+d3X_train, d3X_test, d3y_train, d3y_test = train_test_split(X, Parr3.transform(y), test_size=0.50, random_state=1)
+
 # convert class from strings to integers for training regression models
 array2 = preprocessing.LabelEncoder()
 array2.fit(y)
@@ -25,22 +43,21 @@ array2.transform(y)
 
 # Split data into 2 folds for training and test
 X_train, X_test, y_train, y_test = train_test_split(X, array2.transform(y), test_size=0.50, random_state=1)
-X_train2, X_test2, y_train2, y_test2 = train_test_split(X, array2.transform(y), test_size=0.50, random_state=1)
-
-# Standard_obj = StandardScaler()
-# Standard_obj.fit(X_train)
-# x_train_std = Standard_obj.transform(X_train)
-# x_test_std = Standard_obj.transform(X_test)
 
 # Polynomial of degree 3 regression
 def Deg3LinReg_Test(x1, x2, y1, y2) :
-  # x = features
-  # y = classes (flowers)
-  PR = PolynomialFeatures(degree=3)
-    
-  print("Polynomial of degree 3 regression: \n")
-  print("Accuracy Score: ",accuracy_score(y_Fold1, pred1)) # accuracy
-  print("Confusion Matrix:\n",confusion_matrix(y_Fold1, pred1)) # confusion matrix
-  print("#########################\n")
+  LR = LinearRegression().fit(x1, y1)
+  pred1 = LR.predict(x2).round() # first fold testing
+  LR.fit(x2, y2) # second fold training
+  pred2 = LR.predict(x1).round() # second fold testing
 
-#Deg3LinReg_Test(X_train, X_test, y_train, y_test) 
+
+  actual = np.concatenate([y2, y1])
+  predicted = np.concatenate([pred1, pred2])
+  predicted = predicted.round()
+    
+  print("Polynomial of degree 3 regression: \n")   
+  print("Accuracy Score: ",accuracy_score(actual, predicted)) # accuracy
+  print("Confusion Matrix:\n",confusion_matrix(actual, predicted)) # confusion matrix
+ 
+Deg3LinReg_Test(d3X_train, d3X_test, d3y_train, d3y_test) 
