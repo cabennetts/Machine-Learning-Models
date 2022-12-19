9eef5f0e1df136444054be8b7353cc3585b462a5
Caleb <48822049+cabennetts@users.noreply.github.com>
2022-12-18 23:30:34 -0600

    added 5 new classifiers
    - Decision Trees
    - Extra Trees
    - Neural Network
    - Random Forests
    - Support Vector Machine

---
+++ NeuralNetwork.py
@@ -0,0 +1,44 @@
+import sys
+import scipy
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import cross_val_score, train_test_split, RepeatedStratifiedKFold, cross_val_score
+from sklearn.neural_network import MLPClassifier # neural network 
+
+# loads the iris data set
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv('./iris.csv', names=names)
+array = dataset.values # turn numpy dataset into array of values
+
+X = array[:,0:4] # contains flower features
+y = array[:,4] # contains flower names
+
+# convert class from strings to integers for training regression models
+array2 = preprocessing.LabelEncoder()
+array2.fit(y)
+array2.transform(y)
+
+# Split data into 2 folds for training and test
+X_train, X_test, y_train, y_test = train_test_split(X, array2.transform(y), test_size=0.50, random_state=1)
+
+# Neural Network
+def NN(x1, x2, y1, y2):
+    # clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,2), random_state=1)
+    clf = MLPClassifier(max_iter=1000)
+    clf.fit(x1,y1)
+    pred1 = clf.predict(x2)
+    clf = clf.fit(x2, y2)
+    pred2 = clf.predict(x1)
+
+    actual = np.concatenate([y2, y1])
+    predicted = np.concatenate([pred1, pred2])
+    predicted = predicted.round()
+
+    print("Neural Network:")
+    print("Accuracy Score: ",accuracy_score(actual, predicted)) # accuracy
+    print("Confusion Matrix:\n",confusion_matrix(actual, predicted)) # confusion matrix
+
+NN(X_train, X_test, y_train, y_test)
---
+++ RandomForest.py
@@ -0,0 +1,43 @@
+import sys
+import scipy
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import cross_val_score, train_test_split, RepeatedStratifiedKFold, cross_val_score
+from sklearn.ensemble import RandomForestClassifier # random forest
+
+# loads the iris data set
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv('./iris.csv', names=names)
+array = dataset.values # turn numpy dataset into array of values
+
+X = array[:,0:4] # contains flower features
+y = array[:,4] # contains flower names
+
+# convert class from strings to integers for training regression models
+array2 = preprocessing.LabelEncoder()
+array2.fit(y)
+array2.transform(y)
+
+# Split data into 2 folds for training and test
+X_train, X_test, y_train, y_test = train_test_split(X, array2.transform(y), test_size=0.50, random_state=1)
+
+# Random Forest
+def RandomForest(x1, x2, y1, y2):
+    clf = RandomForestClassifier()
+    clf = clf.fit(x1, y1)
+    pred1 = clf.predict(x2)
+    clf = clf.fit(x2, y2)
+    pred2 = clf.predict(x1)
+
+    actual = np.concatenate([y2, y1])
+    predicted = np.concatenate([pred1, pred2])
+    predicted = predicted.round()
+    
+    print("Random Forest:")
+    print("Accuracy Score: ",accuracy_score(actual, predicted)) # accuracy
+    print("Confusion Matrix:\n",confusion_matrix(actual, predicted)) # confusion matrix
+
+RandomForest(X_train, X_test, y_train, y_test)
